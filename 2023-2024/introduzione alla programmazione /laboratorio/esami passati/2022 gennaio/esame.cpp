be71ea01822a67a18c649890033d64977cb458f0
Giulia Ponassi <giulia.ponassi@gmail.com>
2024-11-13 11:09:15 +0100

    Update esame.cpp

--- 2023-2024/introduzione alla programmazione /laboratorio/esami passati/2022 gennaio/esame.cpp
+++ 2023-2024/introduzione alla programmazione /laboratorio/esami passati/2022 gennaio/esame.cpp
@@ -19,19 +19,16 @@ void add ( BagOfWords & b , std :: string w ) {
     }
 }
 
-void del ( BagOfWords & b , std :: string w ) {
+void del ( BagOfWords & b, string w ) {
+    unsigned int count = 0;
     for (unsigned int i = 0; i < b.size(); ++i) {
-        if (b[i].word == w) {
-            if (b[i].num == 1) {
-                for (unsigned int j = i; j < b.size(); ++j) {
-                    b[j].word = b[j + 1].word;
-                }
-                b.pop_back();
-            } else if (b[i].num > 1) {
-                --b[i].num;
-            } else {
-                return;
+        if (b[i].word == w && b[i].num > 1) {
+            --b[i].num;
+        } else if (b[i].word == w && b[i].num == 1) {
+            for (int k = i; k < b.size(); ++k) {
+                b[k].word = b[k + 1].word;
             }
+            b.pop_back();
         }
     }
 }
